2d186e3306c6e2bd06bba231d457511c5655987d
shadowmanos <manos@shadowrobot.com>
2013-09-05 10:01:27 +0100

    removed folders auto-generated by catkin_make

--- build/catkin_generated/order_packages.py
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/manos/Documents/shadow_workspaces/sr_ronex_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
