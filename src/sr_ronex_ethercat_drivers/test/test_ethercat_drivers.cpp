74c07c9219fb3d17c0258116630fac06a5145baf
shadowmanos <manos@shadowrobot.com>
2013-09-11 19:18:44 +0100

    added tests for sr_ronex_ethercat_drivers

--- src/sr_ronex_ethercat_drivers/test/test_ethercat_drivers.cpp
+++ src/sr_ronex_ethercat_drivers/test/test_ethercat_drivers.cpp
@@ -26,33 +26,54 @@
 #include <stdint.h>
 #include <sr_ronex_external_protocol/Ronex_Protocol_0x02000001_GIO_00.h>
 #include <sr_ronex_ethercat_drivers/ronex_utils.hpp>
+#include <al/ethercat_slave_handler.h>
 #include <ros/ros.h>
 #include <gtest/gtest.h>
 
+using namespace std;
+using namespace ronex;
 
 TEST(RonexUtils, set_bit)
 {
   int32u data = 0;
-  ronex::set_bit(data, 1, true);
+  set_bit(data, 1, true);
   EXPECT_EQ(data, 2);
 
-  ronex::set_bit(data, 1, false);
+  set_bit(data, 1, false);
   EXPECT_EQ(data, 0);
 
-  ronex::set_bit(data, 1, true);
-  ronex::set_bit(data, 2, true);
-  ronex::set_bit(data, 3, true);
+  set_bit(data, 1, true);
+  set_bit(data, 2, true);
+  set_bit(data, 3, true);
   EXPECT_EQ(data, 14);
 
   for ( size_t i = 0; i < 32; ++i )
-    ronex::set_bit(data, i, true);
+    set_bit(data, i, true);
   EXPECT_EQ(data, 4294967295);
 
-  ronex::set_bit(data, 1, true);
-  ASSERT_TRUE( ronex::check_bit(data, 1 ) );
+  set_bit(data, 1, true);
+  ASSERT_TRUE( check_bit(data, 1 ) );
 
-  ronex::set_bit(data, 4, false);
-  ASSERT_FALSE( ronex::check_bit(data, 4 ) );
+  set_bit(data, 4, false);
+  ASSERT_FALSE( check_bit(data, 4 ) );
+}
+
+TEST(RonexUtils, build_name )
+{
+  const EC_UDINT serial = 55662211;
+  EtherCAT_FMMU_Config *fmmu = new EtherCAT_FMMU_Config(0);
+  EtherCAT_PD_Config *pdcfg = new EtherCAT_PD_Config(0);
+  EtherCAT_SlaveHandler *sh = new EtherCAT_SlaveHandler( 0, 0, 0, serial, EC_FixedStationAddress( (EC_UINT) 0 ), fmmu, pdcfg, 0 );
+
+  ostringstream ostr;
+  ostr << "ronex_0_" << serial;
+  string result = build_name(sh);
+
+  ASSERT_STREQ( result.c_str(), ostr.str().c_str() );
+
+  delete fmmu;
+  delete pdcfg;
+  delete sh;
 }
 
 int main(int argc, char **argv)
