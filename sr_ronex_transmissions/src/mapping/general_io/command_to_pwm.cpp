4526ca46a11bba11c1f041a097c009819aa61a75
Toni Oliver <toni.oliver.d@gmail.com>
2013-09-18 14:27:16 +0100

    Adds motor direction pin

--- sr_ronex_transmissions/src/mapping/general_io/command_to_pwm.cpp
+++ sr_ronex_transmissions/src/mapping/general_io/command_to_pwm.cpp
@@ -1,25 +1,25 @@
 /*
- * Copyright (c) 2013, Shadow Robot Company, All rights reserved.
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.
- */
+* Copyright (c) 2013, Shadow Robot Company, All rights reserved.
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 3.0 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library.
+*/
 
 /**
- * @file   command_to_pwm.hpp
- * @author Ugo Cupcic <ugo@shadowrobot.com>
- * @brief  Contains the data mapping one joint command to a pwm module.
- **/
+* @file command_to_pwm.hpp
+* @author Ugo Cupcic <ugo@shadowrobot.com>
+* @brief Contains the data mapping one joint command to a pwm module.
+**/
 
 #include "sr_ronex_transmissions/mapping/general_io/command_to_pwm.hpp"
 #include <pr2_mechanism_model/robot.h>
@@ -159,7 +159,7 @@ namespace ronex
   }
 }
 /* For the emacs weenies in the crowd.
-   Local Variables:
-   c-basic-offset: 2
-   End:
+Local Variables:
+c-basic-offset: 2
+End:
 */
