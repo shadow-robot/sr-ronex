f99ac5620c7ba6600fea1948d67d34811c892f22
Yi Li <yi@shadowrobot.com>
2013-09-19 13:12:30 +0100

    Add license text to C++ files.

--- sr_ronex_drivers/src/cod_decod/cod_decod_manager.cpp
+++ sr_ronex_drivers/src/cod_decod/cod_decod_manager.cpp
@@ -1,22 +1,29 @@
+/*
+ * Copyright (c) 2013, Shadow Robot Company, All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
 /**
  * @file   cod_decod_manager.cpp
  * @author Toni Oliver <toni@shadowrobot.com>
- *
-* Copyright 2013 Shadow Robot Company Ltd.
-*
-* This program is Proprietary software: you cannot redistribute it or modify it
-*
-* This program is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-* FITNESS FOR A PARTICULAR PURPOSE.
-*
-*
- * @brief An instance of this class will be created by the driver of every ethercat slave.
- * Depending on the configuration read from the parameter server (defined in cod_decod_config.yaml) It will instantiate an object of the
- * corresponding subclass of CodDecod.
- * The update and build_command functions will call the corresponding functions of the CodDecod object.
- *
- */
+ * @brief  An instance of this class will be created by the driver of every ethercat slave.
+ *         Depending on the configuration read from the parameter server 
+ *         (defined in cod_decod_config.yaml) It will instantiate an object of the
+ *         corresponding subclass of CodDecod. The update and build_command functions will
+ *         call the corresponding functions of the CodDecod object.
+ **/
 
 #include "sr_ronex_drivers/cod_decod/cod_decod_manager.hpp"
 #include "sr_ronex_drivers/cod_decod/cod_decod_std_io.hpp"
