a3c68e8b0269e6c2a7987a8695c2108f5dcc289b
Ugo Cupcic <ugo@shadowrobot.com>
2013-09-19 09:09:51 +0200

    Using conventions: CamelCasedClasses, private_, indentation, etc...

--- sr_ronex_examples/src/sr_ronex_parse_parameter_servers.cpp
+++ sr_ronex_examples/src/sr_ronex_parse_parameter_servers.cpp
@@ -27,85 +27,87 @@
 
 //-------------------------------------------------------------------------------
 
-class SR_RONEX_Example
+class SrRonexExample
 {
 public:
-  SR_RONEX_Example() {}
-  ~SR_RONEX_Example() {}
-  
+  SrRonexExample() {}
+  ~SrRonexExample() {}
+
 public:
   void run(void)
   {
-    this->find_ronexes();
+    find_ronexes_();
   }
 
 private:
-  void find_ronexes(void)
-  { 
+  void find_ronexes_(void)
+  {
     ros::Rate loop_rate(10);
     std::string param;
-    while ( ros::param::get("/ronex/0/ronex_id", param ) == false ) {
+    while ( ros::param::get("/ronex/0/ronex_id", param ) == false )
+    {
       ROS_INFO( "Waiting for the ronex to be loaded properly." );
       loop_rate.sleep();
     }
-    
-    /* 
-     * Checks the ronexes already present on the parameter server and returns 
+
+    /*
+     * Checks the ronexes already present on the parameter server and returns
      * an id on which the given ronex is stored on the parameter server.
-     * The method returns the index of the ronex in the parameter server. 
+     * The method returns the index of the ronex in the parameter server.
      * -1 if not found. Or the next free index if ronex_id == "".
      */
     std::string ronex_id("");
     int next_free_index = ronex::get_ronex_param_id(ronex_id);
     for (int k = 0; k < next_free_index; k++)
+    {
+      ronex_id = to_string_(k);
+      int ronex_parameter_id = ronex::get_ronex_param_id(ronex_id);
+      if ( ronex_parameter_id == -1 )
       {
-	ronex_id = this->to_string(k);
-	int ronex_parameter_id = ronex::get_ronex_param_id(ronex_id);
-	if ( ronex_parameter_id == -1 ) {
-	  ROS_INFO( "Did not find the ronex with ronex_id %s.", ronex_id.c_str() );
-	  continue;
-	}
-	
-	std::string product_id;
-	std::string product_id_key = this->get_key( ronex_parameter_id, std::string("product_id") );
-	ros::param::get( product_id_key, product_id );
-	ROS_INFO( "product_id = %s.", product_id.c_str() );
-	
-	std::string product_name;
-	std::string product_name_key = this->get_key( ronex_parameter_id, std::string("product_name") );
-	ros::param::get( product_name_key, product_name );
-	ROS_INFO( "product_name = %s.", product_name.c_str() );
-	
-	std::string ronex_id;
-	std::string ronex_id_key = this->get_key( ronex_parameter_id, std::string("ronex_id") );
-	ros::param::get( ronex_id_key, ronex_id );
-	ROS_INFO( "ronex_id = %s.", ronex_id.c_str() );
-	
-	std::string path;
-	std::string path_key = this->get_key( ronex_parameter_id, std::string("path") );
-	ros::param::get( path_key, path );
-	ROS_INFO( "path = %s.", path.c_str() );
-	
-	std::string serial;
-	std::string serial_key = this->get_key( ronex_parameter_id, std::string("serial") );
-	ros::param::get( serial_key, serial );
-	ROS_INFO( "serial = %s.", serial.c_str() );		
+        ROS_INFO( "Did not find the ronex with ronex_id %s.", ronex_id.c_str() );
+        continue;
       }
+
+      std::string product_id;
+      std::string product_id_key = get_key_( ronex_parameter_id, std::string("product_id") );
+      ros::param::get( product_id_key, product_id );
+      ROS_INFO( "product_id = %s.", product_id.c_str() );
+
+      std::string product_name;
+      std::string product_name_key = get_key_( ronex_parameter_id, std::string("product_name") );
+      ros::param::get( product_name_key, product_name );
+      ROS_INFO( "product_name = %s.", product_name.c_str() );
+
+      std::string ronex_id;
+      std::string ronex_id_key = get_key_( ronex_parameter_id, std::string("ronex_id") );
+      ros::param::get( ronex_id_key, ronex_id );
+      ROS_INFO( "ronex_id = %s.", ronex_id.c_str() );
+
+      std::string path;
+      std::string path_key = get_key_( ronex_parameter_id, std::string("path") );
+      ros::param::get( path_key, path );
+      ROS_INFO( "path = %s.", path.c_str() );
+
+      std::string serial;
+      std::string serial_key = get_key_( ronex_parameter_id, std::string("serial") );
+      ros::param::get( serial_key, serial );
+      ROS_INFO( "serial = %s.", serial.c_str() );
+    }
   }
-  
+
   // Convert integer d to string s.
-  std::string to_string(int d) 
+  std::string to_string_(int d)
   {
-    std::stringstream ss; 
-    ss << d; 
+    std::stringstream ss;
+    ss << d;
     std::string s(ss.str());
     return s;
   }
 
-  std::string get_key(int ronex_parameter_id, std::string part)
+  std::string get_key_(int ronex_parameter_id, std::string part)
   {
     std::string key("/ronex/");
-    key += this->to_string(ronex_parameter_id);
+    key += to_string_(ronex_parameter_id);
     key += "/";
     key += part;
     return key;
@@ -118,7 +120,7 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "parse_parameter_servers");
 
-  SR_RONEX_Example example;
+  SrRonexExample example;
   example.run();
 
   return 0;
