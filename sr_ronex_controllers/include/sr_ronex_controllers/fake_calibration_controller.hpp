f1e99fcb91a59c7aa2da07046aa7bfed463b1867
Ugo Cupcic <ugo@shadowrobot.com>
2013-09-23 16:13:06 +0200

    added fake calibration controller, wrote example for urdf

---
+++ sr_ronex_controllers/include/sr_ronex_controllers/fake_calibration_controller.hpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2013, Shadow Robot Company, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+/**
+ * @file   fake_calibration_controller.hpp
+ * @author Ugo Cupcic <ugo@shadowrobot.com>
+ * @brief  Those controllers fake the calibration, setting the joints as
+ *         "calibrated" - otherwise the joints can't be controlled.
+ **/
+
+#ifndef _FAKE_CALIBRATION_CONTROLLER_H_
+#define _FAKE_CALIBRATION_CONTROLLER_H_
+
+#include <ros/node_handle.h>
+
+#include <boost/smart_ptr.hpp>
+#include "pr2_mechanism_model/robot.h"
+#include "realtime_tools/realtime_publisher.h"
+#include "std_msgs/Bool.h"
+#include <pr2_controller_interface/controller.h>
+#include <sr_ronex_hardware_interface/mk2_gio_hardware_interface.hpp>
+
+namespace ronex
+{
+  class FakeCalibrationController
+    : public pr2_controller_interface::Controller
+  {
+  public:
+    FakeCalibrationController();
+    virtual ~FakeCalibrationController();
+
+    virtual bool init(pr2_mechanism_model::RobotState* robot, ros::NodeHandle &n);
+
+    /*!
+     * \brief Issues commands to the joint. Should be called at regular intervals
+     */
+    virtual void update();
+
+  private:
+    pr2_mechanism_model::RobotState* robot_;
+    ros::NodeHandle node_;
+    boost::shared_ptr<realtime_tools::RealtimePublisher<std_msgs::Bool> > pub_calibrated_;
+    ros::Time last_publish_time_;
+
+    enum { INITIALIZED, BEGINNING, MOVING_TO_LOW, MOVING_TO_HIGH, CALIBRATED };
+    int state_;
+
+    pr2_mechanism_model::JointState *joint_;
+    std::string joint_name_;
+
+    std_msgs::Bool calib_msg_;
+  };
+}
+
+/* For the emacs weenies in the crowd.
+Local Variables:
+   c-basic-offset: 2
+End:
+*/
+
+#endif /* _FAKE_CALIBRATION_CONTROLLER_H_ */
