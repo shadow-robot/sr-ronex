a16ae3e20fe69f7f205b6b4bbb68bef0fd3b3b1f
Yi Li <yi@shadowrobot.com>
2013-09-24 10:55:19 +0100

    Explain path and how it loos like.

--- sr_ronex_examples/src/sr_ronex_parse_parameter_server.py
+++ sr_ronex_examples/src/sr_ronex_parse_parameter_server.py
@@ -54,6 +54,7 @@ class SrRonexParseParamExample(object):
         devices = rospy.get_param("/ronex/devices")
         for ronex_param_id in devices:
             # Retrieve the values of all parameters related to the current General I/O module.
+            # Path looks like "/ronex/general_io/2", where 2 is a ronex_id.
             rospy.loginfo( "*** General I/O Module %s ***",  ronex_param_id );
             rospy.loginfo( "product_id   = %s", devices[ronex_param_id]["product_id"] );
             rospy.loginfo( "product_name = %s", devices[ronex_param_id]["product_name"] );
