df4a0f5af0135517b3018639f0f2cc6233c27ef8
Ugo Cupcic <ugo@shadowrobot.com>
2013-11-20 11:29:08 +0100

    adding common protocol header for spi and diagram

---
+++ sr_ronex_external_protocol/include/sr_ronex_external_protocol/Ronex_Protocol_0x02000002_SPI_00.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2013, Shadow Robot Company, All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+
+//! EtherCAT protocol for RoNeX SPI stacker, 02.
+
+#ifndef RONEX_PROTOCOL_0x02000002_SPI_H_INCLUDED
+#define RONEX_PROTOCOL_0x02000001_SPI_H_INCLUDED
+
+#include "typedefs_shadow.h"
+
+#if defined(__GNUC__)
+
+#else
+    #define __attribute__(x)
+#endif
+
+#endif
