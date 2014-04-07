eab4bfc30ba5df5e13c2396ceacf1490ece0d32d
Ugo Cupcic <ugo@shadowrobot.com>
2014-04-07 09:46:52 +0200

    fixing Hugo's indents

--- sr_ronex_drivers/src/sr_tcat.cpp
+++ sr_ronex_drivers/src/sr_tcat.cpp
@@ -241,10 +241,10 @@ bool SrTCAT::unpackState(unsigned char *this_buffer, unsigned char *prev_buffer)
       for(size_t i=0 ; i<PAYLOAD_MAX_SIZE; ++i)
         state_msg_.received_data[status_data->receiver_number].payload[i] = status_data->receiver_data.payload[i];
 
-      state_msg_.received_data[status_data->receiver_number].rx_frame_information =                          status_data->receiver_data.rx_frame_information;
-      state_msg_.received_data[status_data->receiver_number].std_noise            =                          status_data->receiver_data.std_noise;
-      state_msg_.received_data[status_data->receiver_number].flags                =                          status_data->receiver_data.flags;
-      state_msg_.received_data[status_data->receiver_number].FPI                  = FPI_FIXED_POINT_TO_FLOAT(status_data->receiver_data.FPI);
+      state_msg_.received_data[status_data->receiver_number].rx_frame_information = status_data->receiver_data.rx_frame_information;
+      state_msg_.received_data[status_data->receiver_number].std_noise = status_data->receiver_data.std_noise;
+      state_msg_.received_data[status_data->receiver_number].flags = status_data->receiver_data.flags;
+      state_msg_.received_data[status_data->receiver_number].FPI = FPI_FIXED_POINT_TO_FLOAT(status_data->receiver_data.FPI);
 
       timestamp_64   = status_data->receiver_data.timestamp_H;
       timestamp_64 <<= 32;
@@ -253,7 +253,7 @@ bool SrTCAT::unpackState(unsigned char *this_buffer, unsigned char *prev_buffer)
       state_msg_.received_data[status_data->receiver_number].timestamp_ns = static_cast<double>(timestamp_64) * (15.65/1000.0);
       //printf("0x%04x%08x = %f\n", status_data->receiver_data.timestamp_H, status_data->receiver_data.timestamp_L, state_msg_.received_data[status_data->receiver_number].timestamp_ns);
       //state_msg_.received_data[status_data->receiver_number].timestamp_ns = static_cast<double>(status_data->receiver_data.timestamp_L + (static_cast<u_int64_t>(status_data->receiver_data.timestamp_H) << 32)*(15.65/1000.0));
-    } //end first time, the sizes are properly initialised, simply fill in the data
+    }
 
   }
 
@@ -268,11 +268,11 @@ bool SrTCAT::unpackState(unsigned char *this_buffer, unsigned char *prev_buffer)
       state_publisher_->msg_ = state_msg_;
       state_publisher_->unlockAndPublish();
     }
-      
+
     //reset the data received flags to false
     for(size_t i=0; i<NUM_RECEIVERS; ++i)
       state_msg_.received_data[status_data->receiver_number].data_received = false;
-      
+
       previous_sequence_number_ = status_data->sequence_number;
   }
 
