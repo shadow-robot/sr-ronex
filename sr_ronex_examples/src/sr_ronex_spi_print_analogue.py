6ffa345e473d42dd71a76995f8dfe2640daec9ef
shadowmanos <manos@shadowrobot.com>
2014-05-29 13:49:11 +0100

    added single joint control from spi analog

--- sr_ronex_examples/src/sr_ronex_spi_print_analogue.py
+++ sr_ronex_examples/src/sr_ronex_spi_print_analogue.py
@@ -20,14 +20,33 @@
 import rospy
 from time import sleep
 from sr_ronex_msgs.srv import SPI
+from std_msgs.msg import Float64
+from math import radians
 
-rospy.init_node('sr_ronex_spi')
-waitress = rospy.ServiceProxy("/ronex/spi/1385468312/command/passthrough/0", SPI)
+class SpiRobotConnector():
+    """
+    retrieves values from sliders connected to spi
+    and publishes as command to the hand
+    """
+    def __init__(self):
+        self.waitress = rospy.ServiceProxy("/ronex/spi/1385468312/command/passthrough/0", SPI)
+        self.pub = rospy.Publisher("sh_ffj0_position_controller/command", Float64, latch=True)
 
-while not rospy.is_shutdown():
-    resp = waitress([6,0,0]).data
-    left = '{:08b}'.format(int(resp[1]))[4:]
-    righ = '{:08b}'.format(int(resp[2]))
-    print(int(left + righ, 2))
-    sleep(0.25)
+    def get_spi_value(self):
+        resp = self.waitress([6,0,0]).data
+        left = '{:08b}'.format(int(resp[1]))[4:]
+        righ = '{:08b}'.format(int(resp[2]))
+        return int(left + righ, 2)
 
+    def execute(self):
+        while not rospy.is_shutdown():
+            value = self.get_spi_value()
+            print("analogue value = {}".format(value))
+            print("radians for hand = {}".format(float(value)))
+            self.pub.publish(radians(float(value)))
+            sleep(0.05)
+
+if __name__ == '__main__':
+    rospy.init_node('sr_ronex_spi')
+    connector = SpiRobotConnector()
+    connector.execute()
