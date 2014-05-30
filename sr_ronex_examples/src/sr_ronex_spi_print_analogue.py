4e0bef34fa8d4875eb427e509244b9c7c54cc339
Shadow Hand at Akron <hand@shadow-akron.(none)>
2014-05-30 04:09:51 -0500

    mod example

--- sr_ronex_examples/src/sr_ronex_spi_print_analogue.py
+++ sr_ronex_examples/src/sr_ronex_spi_print_analogue.py
@@ -21,7 +21,6 @@ import rospy
 from time import sleep
 from sr_ronex_msgs.srv import SPI
 from std_msgs.msg import Float64
-from math import radians
 
 class SpiRobotConnector():
     """
@@ -30,23 +29,43 @@ class SpiRobotConnector():
     """
     def __init__(self):
         self.waitress = rospy.ServiceProxy("/ronex/spi/1385468312/command/passthrough/0", SPI)
-        self.pub = rospy.Publisher("sh_ffj0_position_controller/command", Float64, latch=True)
 
-    def get_spi_value(self):
-        resp = self.waitress([6,0,0]).data
-        left = '{:08b}'.format(int(resp[1]))[4:]
-        righ = '{:08b}'.format(int(resp[2]))
-        return int(left + righ, 2)
+        self.pubs = [
+                rospy.Publisher("/sh_lfj3_position_controller/command", Float64, latch=True),
+                rospy.Publisher("/sh_rfj3_position_controller/command", Float64, latch=True),
+                rospy.Publisher("/sh_mfj3_position_controller/command", Float64, latch=True),
+                rospy.Publisher("/sh_ffj3_position_controller/command", Float64, latch=True)]
+
+    def get_spi_values(self):
+        responses = [
+                self.waitress([6,128,0]).data,  # 18
+                self.waitress([6,192,0]).data,  # 26
+                self.waitress([6,0,0]).data,    # 2
+                self.waitress([6,64,0]).data]   # 10
+
+        results = ['{0:08b}{1:08b}'.format(int(res[1]), int(res[2])) for res in responses]
+
+        return [int(res[4:], 2) for res in results]
 
     def execute(self):
         while not rospy.is_shutdown():
-            value = self.get_spi_value()
-            print("analogue value = {}".format(value))
-            print("radians for hand = {}".format(float(value)))
-            self.pub.publish(radians(float(value)))
-            sleep(0.05)
+            values = self.get_spi_values()
+            print("analogs = [ {0:6} {1:6} {2:6} {3:6} ]".
+                    format(values[0], values[1], values[2], values[3]))
+
+            for val, pub in zip(values, self.pubs):
+                # scale   3500 -> 500
+                # to      pi/2 ->   0
+                # (pi/2)/(3500-500) = 
+                command = 0.000523599*(float(val) - 500.0)
+                pub.publish(command)
+            #print("publish 0.5")
+            #self.pubs[1].publish(0.5)    
+
+            sleep(0.2)
 
 if __name__ == '__main__':
     rospy.init_node('sr_ronex_spi')
     connector = SpiRobotConnector()
     connector.execute()
+
