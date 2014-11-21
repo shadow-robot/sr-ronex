1a27f5ea20bca5b7fb7fdc872127819a18be9496
Ugo Cupcic <ugo@shadowrobot.com>
2014-11-21 16:12:25 +0100

    Sending packet and checking response per spi channel.

--- sr_ronex_test/test/sr_spi_test.py
+++ sr_ronex_test/test/sr_spi_test.py
@@ -84,18 +84,40 @@ class TestSPIWithHardware(unittest.TestCase):
       self.assertTrue(ctrl.name in self.controllers_list, msg="Available controllers: "+str(available_controllers.controller) + " / expected controller: " +str(self.controllers_list) )
 
   def test_all_cases(self):
-    self.run_test_case(0, 0)
+    # in the document
+    # expected_A0, expected_A1, expected_A2, expected_A3,
+    # expected_A4, expected_A5, expected_PA, expected_PD)
+    #
+    # we expect in the function:
+    # expected: [[A5 A1 PD A3][A4 P0 PA A2]]
+
+    #Test 1
+    self.run_test_case([True, True, True, True, True],
+                       [[2048, 2048, 2048, 2048], [2048, 2048, 2048, 2048]])
+
+    #Test 2
+    self.run_test_case([False, True, True, True, True],)
 
   def run_test_case(self, digital_states, expected_As):
     success = True
-    
+
+    # expected: A5 A1 PD A3
+    results = []
     for adc_number in range(len(self.spi_srv)):
-      self.read_adc(adc_number, 0)
-    
-  
+      results.append( self.read_adc(adc_number, 0) )
+
+    self.assertAlmostEquals(results, expected_As[0], msg="Testing channel 0 failed.", delta=30)
+
+    # expected: A4 P0 PA A2
+    results = []
+    for adc_number in range(len(self.spi_srv)):
+      results.append( self.read_adc(adc_number, 1) )
+
+    self.assertAlmostEquals(results, expected_As[1], msg="Testing channel 1 failed.", delta=30)
+
   def read_adc(self, adc_number, channel_number):
     req = SPIRequest()
-    
+
     # set pre / post states
     cfg = {"pin_output_state_pre_DIO_0": True, "pin_output_state_post_DIO_0": False,
            "pin_output_state_pre_DIO_1": True, "pin_output_state_post_DIO_1": False,
@@ -103,12 +125,12 @@ class TestSPIWithHardware(unittest.TestCase):
            "pin_output_state_pre_DIO_3": True, "pin_output_state_post_DIO_3": False,
            "pin_output_state_pre_DIO_4": True, "pin_output_state_post_DIO_4": False,
            "pin_output_state_pre_DIO_5": True, "pin_output_state_post_DIO_5": False}
-    
+
     cfg["pin_output_state_pre_DIO_" + str(adc_number)] = False
     cfg["pin_output_state_post_DIO_" + str(adc_number)] = True
-        
+
     self.dyn_rcf_client.update_configuration(cfg)
-    
+
     # set data
     req.data = [0x0]*3
     req.data[0] = 0x01
@@ -116,14 +138,23 @@ class TestSPIWithHardware(unittest.TestCase):
       req.data[1] = 0xA0
     else:
       req.data[1] = 0xB0
-    req.data[2] = 0x01  
-      
+    req.data[2] = 0x01
+
     spi_res = None
-    try:
-      spi_res = self.spi_srv[adc_number](req)
-    except rospy.ServiceException as exc:
-      self.assertTrue(False, "Failed to send data to "+str(adc_number) +
-                       " channel "+str(channel_number)+" -> "+ str(exc)   )
+    # The SPI respons is generated only after the first packet is
+    # sent so we need to send a message twice.
+    for i in range(2):
+      try:
+        spi_res = self.spi_srv[adc_number](req)
+      except rospy.ServiceException as exc:
+        self.assertTrue(False, "Failed to send data to "+str(adc_number) +
+                        " channel "+str(channel_number)+" -> "+ str(exc)   )
+
+    result = hex(spi_res.data[1])
+    result <<= 8
+    result |= hex(spi_res.data[2])
+    result &= 0x0FFF
+    return result
 
 ############################################
 # TEST END
