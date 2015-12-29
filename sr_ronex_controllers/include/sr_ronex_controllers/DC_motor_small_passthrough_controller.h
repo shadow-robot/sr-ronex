d9ad824a6d76b8232ccc6811ccaa9fa4e4c395bd
Toni Oliver <toni@shadowrobot.com>
2015-12-29 17:59:02 +0000

    skeleton of passthrough controller for motor controller

---
+++ sr_ronex_controllers/include/sr_ronex_controllers/DC_motor_small_passthrough_controller.h
@@ -0,0 +1,51 @@
+/*
+ * DC_motor_small_controller.h
+ *
+ *  Created on: 29 Dec 2015
+ *      Author: hand
+ */
+
+#ifndef SR_RONEX_SR_RONEX_CONTROLLERS_INCLUDE_DC_MOTOR_SMALL_CONTROLLER_H_
+#define SR_RONEX_SR_RONEX_CONTROLLERS_INCLUDE_DC_MOTOR_SMALL_CONTROLLER_H_
+
+#include <ros/node_handle.h>
+
+#include <controller_interface/controller.h>
+#include <ros_ethercat_model/robot_state.hpp>
+#include <sr_ronex_hardware_interface/mk2_gio_hardware_interface.hpp>
+#include <realtime_tools/realtime_publisher.h>
+#include <sr_ronex_utilities/sr_ronex_utilities.hpp>
+
+#include <std_msgs/Bool.h>
+#include <sr_ronex_msgs/PWM.h>
+#include <vector>
+
+namespace ronex
+{
+class DCMotorSmallPassthroughController
+    : public controller_interface::Controller<ros_ethercat_model::RobotState>
+{
+public:
+  DCMotorSmallPassthroughController();
+
+  virtual bool init(ros_ethercat_model::RobotState* robot, ros::NodeHandle &n);
+
+  /*!
+   * \brief Issues commands to the joint. Should be called at regular intervals
+   */
+  virtual void update(const ros::Time&, const ros::Duration&) {}
+
+  void digital_commands_cb(const std_msgs::BoolConstPtr& msg, int index);
+
+private:
+  ros::NodeHandle node_;
+
+  int loop_count_;
+
+  ronex::DCMotor* dc_motor_small_;
+
+  /// send commands to the RoNeX's digital I/O
+  std::vector<ros::Subscriber> digital_subscribers_;
+};
+}  // namespace ronex
+#endif /* SR_RONEX_SR_RONEX_CONTROLLERS_INCLUDE_DC_MOTOR_SMALL_CONTROLLER_H_ */
---
+++ sr_ronex_controllers/src/DC_motor_small_passthrough_controller.cpp
@@ -0,0 +1,16 @@
+/*
+ * DCMotorSmallController.cpp
+ *
+ *  Created on: 29 Dec 2015
+ *      Author: hand
+ */
+
+#include <sr_ronex_controllers/DC_motor_small_passthrough_controller.h>
+
+namespace ronex
+{
+DCMotorSmallPassthroughController::DCMotorSmallPassthroughController()
+:loop_count_(0)
+{
+}
+}  // namespace ronex
